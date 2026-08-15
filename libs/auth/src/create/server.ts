ab814f10930a3259b492036c9e3647a27b162adb
Ryan Luong <lumintriet@gmail.com>
2026-08-15 20:56:16 +0700

    feat(auth/create): define create functions.

---
+++ libs/auth/src/create/server.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth/minimal";
+
+import type { AuthServerParams } from "@/create-server-options";
+
+import { createAuthServerOptions } from "@/create-server-options";
+
+export function createAuthServer({
+  db,
+  baseURL,
+  basePath,
+  secret,
+}: AuthServerParams) {
+  return betterAuth(
+    createAuthServerOptions({
+      db,
+      baseURL,
+      basePath,
+      secret,
+    }),
+  );
+}
---
+++ libs/auth/src/create/tanstack-start-server.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth/minimal";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+import type { AuthServerParams } from "@/create-server-options";
+
+import { createAuthServerOptions } from "@/create-server-options";
+
+export function createTanstackStartAuthServer({
+  db,
+  baseURL,
+  basePath,
+  secret,
+}: AuthServerParams) {
+  return betterAuth({
+    ...createAuthServerOptions({
+      db,
+      baseURL,
+      basePath,
+      secret,
+    }),
+    // Make sure to keep the tanstackStartCookies plugin at the end of the array.
+    plugins: [tanstackStartCookies()],
+  });
+}
