71acbb858dfed189e33f802a1ac15c01c8371130
Ryan Luong <lumintriet@gmail.com>
2026-08-15 20:23:53 +0700

    feat(auth): define createAuthServerOptions function.

--- libs/auth/src/constants.ts
+++ libs/auth/src/constants.ts
@@ -1,1 +1,4 @@
 export const DEFAULT_AUTH_BASE_PATH = "/api/auth";
+
+export const MIN_PASSWORD_LENGTH = 12;
+export const MAX_PASSWORD_LENGTH = 128;
---
+++ libs/auth/src/create-server-options.ts
@@ -0,0 +1,52 @@
+import type { Db } from "@repo/db";
+import type { BetterAuthOptions } from "better-auth/minimal";
+
+import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
+import { accountTable } from "@repo/db/schema/tables/account";
+import { sessionTable } from "@repo/db/schema/tables/session";
+import { userTable } from "@repo/db/schema/tables/user";
+import { verificationTable } from "@repo/db/schema/tables/verification";
+
+import type { AuthBasePath, AuthBaseUrl, AuthSecret } from "@/env";
+
+import { MAX_PASSWORD_LENGTH, MIN_PASSWORD_LENGTH } from "@/constants";
+
+export interface AuthServerParams {
+  db: Db;
+  baseURL: AuthBaseUrl;
+  basePath: AuthBasePath;
+  secret: AuthSecret;
+}
+
+export function createAuthServerOptions({
+  db,
+  baseURL,
+  basePath,
+  secret,
+}: AuthServerParams) {
+  return {
+    baseURL,
+    basePath,
+    secret,
+    database: drizzleAdapter(db, {
+      provider: "pg",
+      schema: {
+        user: userTable,
+        session: sessionTable,
+        account: accountTable,
+        verification: verificationTable,
+      },
+    }),
+    advanced: {
+      database: {
+        generateId: false,
+        joins: true,
+      },
+    },
+    emailAndPassword: {
+      enabled: true,
+      minPasswordLength: MIN_PASSWORD_LENGTH,
+      maxPasswordLength: MAX_PASSWORD_LENGTH,
+    },
+  } satisfies BetterAuthOptions;
+}
