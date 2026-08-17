b5e5beab7153b88dc2a9a76f012e2484eab35756
Ryan Luong <lumintriet@gmail.com>
2026-08-17 09:51:36 +0700

    feat(web/auth): make api route return response on error onstead of throwing redirect.

--- apps/web/src/routes/api/auth/$.ts
+++ apps/web/src/routes/api/auth/$.ts
@@ -1,3 +1,4 @@
+import { HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD } from "@repo/http/response-statuses/error/server";
 import { createFileRoute } from "@tanstack/react-router";
 
 import { getAuthServer } from "@/features/auth/ops/get-server.server";
@@ -5,13 +6,17 @@ import { getAuthServer } from "@/features/auth/ops/get-server.server";
 export const Route = createFileRoute("/api/auth/$")({
   server: {
     handlers: {
-      GET: async ({ request }) => {
-        const authServer = getAuthServer();
-        return await authServer.handler(request);
-      },
-      POST: async ({ request }) => {
-        const authServer = getAuthServer();
-        return await authServer.handler(request);
+      ANY: async ({ request }) => {
+        const getAuthServerResult = getAuthServer();
+        if (!getAuthServerResult.ok) {
+          const httpError =
+            HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD.INTERNAL_SERVER_ERROR;
+          return Response.json(getAuthServerResult.error.shallowSerialize(), {
+            status: httpError.code,
+            statusText: httpError.text,
+          });
+        }
+        return await getAuthServerResult.data.handler(request);
       },
     },
   },
