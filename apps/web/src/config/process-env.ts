67b581e5273003628e21ef55a0e80ead8d29e2b2
Ryan Luong <lumintriet@gmail.com>
2026-08-17 08:06:16 +0700

    feat(web/config): define getProcessEnv function.

--- apps/web/src/config/process-env.ts
+++ apps/web/src/config/process-env.ts
@@ -1,8 +1,29 @@
+import type { Result } from "@repo/result/types";
+
 import { AuthSecretEnvSchema } from "@repo/auth/schemas/env/secret";
 import { DbEnvSchema } from "@repo/db/env";
+import { InvalidEnvError } from "@repo/env/error";
+import { err, ok } from "@repo/result/utils";
 import { z } from "zod";
 
 export const ProcessEnvSchema = z.object({
   ...DbEnvSchema.shape,
   ...AuthSecretEnvSchema.shape,
 });
+export type ProcessEnv = z.infer<typeof ProcessEnvSchema>;
+
+export function getProcessEnv(): Result<ProcessEnv, InvalidEnvError> {
+  const parseEnvResult = ProcessEnvSchema.safeParse(process.env);
+
+  if (!parseEnvResult.success) {
+    return err(
+      new InvalidEnvError({
+        message:
+          "Something went wrong. Please try again later or contact support.",
+        cause: parseEnvResult.error,
+      }),
+    );
+  }
+
+  return ok(parseEnvResult.data);
+}
