091e448ac26c35fb606b39041e0b89ecc736ad28
Ryan Luong <lumintriet@gmail.com>
2026-08-17 09:41:50 +0700

    feat(web/auth): make getAuthServer function return a Result type instead of throwing redirect on error.

--- apps/web/src/features/auth/ops/get-server.server.ts
+++ apps/web/src/features/auth/ops/get-server.server.ts
@@ -1,20 +1,27 @@
+import type { TanstackStartAuthServer } from "@repo/auth/types";
+import type { InvalidEnvError } from "@repo/env/error";
+import type { Result } from "@repo/result/types";
+
 import { createTanstackStartAuthServer } from "@repo/auth/create-server/tanstack-start";
 import { createDb } from "@repo/db/create";
-import { redirect } from "@tanstack/react-router";
+import { ok } from "@repo/result/utils";
 import { createServerOnlyFn } from "@tanstack/react-start";
 
 import { getProcessEnv } from "@/config/process-env.server";
 
-export const getAuthServer = createServerOnlyFn(() => {
-  const getProcessEnvResult = getProcessEnv();
-  if (!getProcessEnvResult.ok) {
-    throw redirect({ to: "/500" });
-  }
-  const processEnv = getProcessEnvResult.data;
+export const getAuthServer = createServerOnlyFn(
+  (): Result<TanstackStartAuthServer, InvalidEnvError> => {
+    const getProcessEnvResult = getProcessEnv();
+    if (!getProcessEnvResult.ok) {
+      return getProcessEnvResult;
+    }
+    const processEnv = getProcessEnvResult.data;
 
-  return createTanstackStartAuthServer({
-    db: createDb(processEnv.NEON_POOLED_CONNECTION_STRING),
-    baseURL: import.meta.env.VITE_AUTH_BASE_URL,
-    secret: processEnv.AUTH_SECRET,
-  });
-});
+    const authServer = createTanstackStartAuthServer({
+      db: createDb(processEnv.NEON_POOLED_CONNECTION_STRING),
+      baseURL: import.meta.env.VITE_AUTH_BASE_URL,
+      secret: processEnv.AUTH_SECRET,
+    });
+    return ok(authServer);
+  },
+);
