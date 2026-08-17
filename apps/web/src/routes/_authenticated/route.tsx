638b4babcc88767312c9189ba52c03dcb1cc1ea0
Ryan Luong <lumintriet@gmail.com>
2026-08-17 10:09:28 +0700

    feat(web/auth): rewrite the route guard.

--- apps/web/src/routes/_authenticated/route.tsx
+++ apps/web/src/routes/_authenticated/route.tsx
@@ -1,9 +1,24 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 import { getUser } from "@/features/auth/ops/get-user.function";
 
 export const Route = createFileRoute("/_authenticated")({
-  beforeLoad: () => getUser(),
+  beforeLoad: async ({ location }) => {
+    const getUserResult = await getUser();
+
+    if (getUserResult.ok) {
+      return getUserResult.data;
+    }
+
+    const error = getUserResult.error;
+    if (error.code === "UNAUTHENTICATED_ERROR") {
+      throw redirect({
+        to: "/sign-in",
+        search: { redirect: location.pathname },
+      });
+    }
+    throw redirect({ to: "/500" });
+  },
   component: AuthenticatedLayout,
 });
 
