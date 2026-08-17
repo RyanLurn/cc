fa13f3c069f1131381664b67f7f65d6f26b9878a
Ryan Luong <lumintriet@gmail.com>
2026-08-17 10:03:45 +0700

    feat(web/auth): rewrite getUser.

--- apps/web/src/features/auth/ops/get-user.function.ts
+++ apps/web/src/features/auth/ops/get-user.function.ts
@@ -1,29 +1,58 @@
+import type { UnauthenticatedError } from "@repo/auth/errors";
+import type { AuthUser } from "@repo/auth/types";
+import type { InvalidEnvError } from "@repo/env/error";
+import type { UnexpectedError } from "@repo/error/classes/unexpected";
+import type { FlatErrorObject } from "@repo/error/types";
+import type { Result } from "@repo/result/types";
+
 import { getAuthUser } from "@repo/auth/ops/get-user";
-import { redirect } from "@tanstack/react-router";
+import { HTTP_CLIENT_ERROR_RESPONSE_STATUS_RECORD } from "@repo/http/response-statuses/error/client";
+import { HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD } from "@repo/http/response-statuses/error/server";
+import { err } from "@repo/result/utils";
 import { createServerFn } from "@tanstack/react-start";
-import { getRequest } from "@tanstack/react-start/server";
+import {
+  getRequestHeaders,
+  setResponseStatus,
+} from "@tanstack/react-start/server";
 
 import { getAuthServer } from "@/features/auth/ops/get-server.server";
 
-export const getUser = createServerFn().handler(async () => {
-  const { headers, url } = getRequest();
-  const authServer = getAuthServer();
+export const getUser = createServerFn().handler(
+  async (): Promise<
+    Result<
+      AuthUser,
+      | FlatErrorObject<InvalidEnvError["code"]>
+      | FlatErrorObject<UnauthenticatedError["code"]>
+      | FlatErrorObject<UnexpectedError["code"]>
+    >
+  > => {
+    const getAuthServerResult = getAuthServer();
+    if (!getAuthServerResult.ok) {
+      const httpError =
+        HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD.INTERNAL_SERVER_ERROR;
+      setResponseStatus(httpError.code, httpError.text);
+      return err(getAuthServerResult.error.shallowSerialize());
+    }
+    const authServer = getAuthServerResult.data;
 
-  const getAuthUserResult = await getAuthUser({
-    authServer,
-    headers,
-  });
-
-  if (getAuthUserResult.ok) {
-    return getAuthUserResult.data;
-  }
-
-  const error = getAuthUserResult.error;
-  if (error.code === "UNAUTHENTICATED_ERROR") {
-    throw redirect({
-      to: "/sign-in",
-      search: { redirect: new URL(url).pathname },
+    const getAuthUserResult = await getAuthUser({
+      authServer,
+      headers: getRequestHeaders(),
     });
-  }
-  throw redirect({ to: "/500" });
-});
+
+    if (getAuthUserResult.ok) {
+      return getAuthUserResult;
+    }
+
+    const error = getAuthUserResult.error;
+    if (error.code === "UNAUTHENTICATED_ERROR") {
+      const httpError = HTTP_CLIENT_ERROR_RESPONSE_STATUS_RECORD.UNAUTHORIZED;
+      setResponseStatus(httpError.code, httpError.text);
+    } else {
+      const httpError =
+        HTTP_SERVER_ERROR_RESPONSE_STATUS_RECORD.INTERNAL_SERVER_ERROR;
+      setResponseStatus(httpError.code, httpError.text);
+    }
+    return err(error.shallowSerialize());
+  },
+);
