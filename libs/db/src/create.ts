e731aa80e4a7aaffc3638f5b77d1f41d396fa245
Ryan Luong <lumintriet@gmail.com>
2026-08-17 07:29:31 +0700

    refactor(db): reorganize code.

---
+++ libs/db/src/create.ts
@@ -0,0 +1,12 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+
+import type { NeonPooledConnectionString } from "@/env";
+
+import { relations } from "@/schema/relations";
+
+export function createDb(connectionString: NeonPooledConnectionString) {
+  const client = neon(connectionString);
+  const db = drizzle({ client, relations });
+  return db;
+}
--- libs/db/src/index.ts
+++ libs/db/src/index.ts
@@ -1,14 +1,3 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-
-import type { NeonPooledConnectionString } from "@/env";
-
-import { relations } from "@/schema/relations";
-
-export function createDb(connectionString: NeonPooledConnectionString) {
-  const client = neon(connectionString);
-  const db = drizzle({ client, relations });
-  return db;
-}
+import type { createDb } from "@/create";
 
 export type Db = ReturnType<typeof createDb>;
