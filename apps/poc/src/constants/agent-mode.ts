efe6f9e481e5eb0a4ceff310cc4f5271ccf756ee
Ryan Luong <lumintriet@gmail.com>
2026-08-22 08:13:52 +0700

    feat(poc/constants): create AI_AGENT_MODE_LIST.

---
+++ apps/poc/src/constants/agent-mode.ts
@@ -0,0 +1,9 @@
+export const AI_AGENT_AUTO_MODE = "AUTO";
+export const AI_AGENT_STANDBY_MODE = "STANDBY";
+export const AI_AGENT_ASLEEP_MODE = "ASLEEP";
+
+export const AI_AGENT_MODE_LIST = [
+  AI_AGENT_AUTO_MODE,
+  AI_AGENT_STANDBY_MODE,
+  AI_AGENT_ASLEEP_MODE,
+] as const;
